487d0a9979a15bd77f8620985e2b94e130852f21
N, Harini <harini.n@cgi.com>
2024-11-13 07:58:25 +0000

    Comitted  code

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,5 @@
-from backend.services.gemini_service import GeminiService, ValidationService
+from services.gemini_service import GeminiService
+from models.gemini import llm 
 
-# Example usage
-if __name__ == "__main__":
-    gemini_service = GeminiService(api_key="api-key", model_url=" ")
-    content = " "
-    validation_response = gemini_service.generate_response(content)
-    
-    # Print the validation response
-    print(validation_response)
+gemini_service = GeminiService()
+print(gemini_service.invoke_service({"data":"What is the capital of India"}))
