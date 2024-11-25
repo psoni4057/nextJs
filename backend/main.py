c1835209a91ae2470731996206d81db6256cbf2d
N, Harini <harini.n@cgi.com>
2024-11-25 07:04:07 +0000

    Updated Code

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,14 @@
-# backend/main.py
-from fastapi import FastAPI
-from backend.api.endpoints import compliance
-import agent.agent
+from agent.agent import Agent
+from services.gemini_service import GeminiService
 
-
-app = FastAPI()
-
-app.include_router(compliance.router, prefix="/api/v1")
-
-@app.get("/")
-async def root():
-    return {"message": "Welcome to the GDPR Validator API"}   
-
-
-agent.agent.invoke_agent()   
+gemini_service = GeminiService()
+agent = Agent(service=gemini_service, db_path='gdbr_Validator.db')
+template = """You are a GDPR expert. Tell if the given data is compliant and the reason.
+                : {data}. Do not use technical words, give easy-to-understand responses.
+                The answer should be in JSON format as follows:
+                compliant: yes/no
+                reason: """
+        
+data = "Today is Monday"  
+result = agent.invoke_service(template, data)
+print(result)
