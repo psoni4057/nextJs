c298a5fcd5bb326846c266f16c54c9cde2848ab3
N, Harini <harini.n@cgi.com>
2024-11-25 07:04:03 +0000

    Updated code

--- backend/agent/agent.py
+++ backend/agent/agent.py
@@ -1,5 +1,44 @@
-from services.gemini_service import GeminiService
+import json
+import sqlite3
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain  
+from backend.services.gemini_service import GeminiService 
 
-def invoke_agent():
-    gemini_service = GeminiService()
-    print(gemini_service.invoke_service({"data":"What is the capital of India"}))
+class Agent:
+    def __init__(self, service: GeminiService, db_path: str):
+        self.service = service
+        self.db_path = db_path
+        self.rules = self._retrieve_rules()
+
+    def _retrieve_rules(self):
+        try:
+            conn = sqlite3.connect(self.db_path)
+            cursor = conn.cursor()
+            query = "SELECT rules FROM gdbr_rules" 
+            cursor.execute(query)
+            rules = cursor.fetchall()
+            conn.close()
+            return rules
+        except Exception as e:
+            print(f"Error retrieving rules from database: {e}")
+            return {}
+
+    def _create_prompt(self, template: str, data: str):
+        try:
+            prompt = template.format(data=data) 
+            return prompt
+        except KeyError as e:
+            print(f"Missing key: {e}")
+            return ""
+        except Exception as e:
+            print(f"Error creating prompt: {e}")
+            return ""
+
+    def invoke_service(self, template: str, data: str):
+        """Create a prompt from the template, invoke the Gemini service, and return the response."""
+        prompt_text = self._create_prompt(template, data)
+        if prompt_text:  
+            response_json = self.service.invoke_service(prompt_text)
+            return response_json
+        else:
+            return "Error in creating prompt"
